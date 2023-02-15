d14a2c93eed043b3b54afbf7f38b4a6211bfcde1
r.vinichenko <r.vinichenko@disoft.dev>
2023-02-16 01:36:59 +0200

    work 15.02.23 (3)

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,24 @@
-const sum = (a, b) => a + b;
-window.sum = sum;
+// Header
+let scrollPos = window.scrollY
+
+const header = document.querySelector(".header")
+const scrollChange = 10
+
+window.addEventListener('scroll', function() {
+  scrollPos = window.scrollY;
+
+  if (scrollPos >= scrollChange) {
+    header.classList.add("header--active")
+  }
+  else {
+    header.classList.remove("header--active")
+  }
+})
+
+window.onload = function(){
+  //hide the preloader
+  document.querySelector(".preloader").style.cssText = `
+  opacity: 0;
+  z-index: -1;
+  `
+}
